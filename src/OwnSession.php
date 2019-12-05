7a137f61abdda18be870bed963614a048087b1b9
Vladimír Náprstek <vladimir.naprstek@i-projekt.cz>
2019-12-05 15:24:17 +0100

    Remove IP adress from validation, as it is confusing (proxies etc.)

--- src/OwnSession.php
+++ src/OwnSession.php
@@ -3,7 +3,6 @@
 namespace Zend\Session\Validator;
 
 use Jenssegers\Agent\Agent;
-use Zend\Http\PhpEnvironment\RemoteAddress;
 use Zend\Session\Validator\ValidatorInterface as SessionValidator;
 
 class OwnSession implements SessionValidator
@@ -16,34 +15,8 @@ class OwnSession implements SessionValidator
     protected $data;
 
     /**
-     * Whether to use proxy addresses or not.
-     *
-     * As default this setting is disabled - IP address is mostly needed to increase
-     * security. HTTP_* are not reliable since can easily be spoofed. It can be enabled
-     * just for more flexibility, but if user uses proxy to connect to trusted services
-     * it's his/her own risk, only reliable field for IP address is $_SERVER['REMOTE_ADDR'].
-     *
-     * @var bool
-     */
-    protected static $useProxy = false;
-
-    /**
-     * List of trusted proxy IP addresses
-     *
-     * @var array
-     */
-    protected static $trustedProxies = [];
-
-    /**
-     * HTTP header to introspect for proxies
-     *
-     * @var string
-     */
-    protected static $proxyHeader = 'HTTP_X_FORWARDED_FOR';
-
-    /**
      * Constructor
-     * get the current user session ID and store it in the session as 'valid data'
+     * get the current user data and store it in the session as 'valid data'
      *
      * @param null|string $data
      */
@@ -61,7 +34,7 @@ class OwnSession implements SessionValidator
     protected function createData()
     {
         $agent = new Agent();
-        return session_id() . '|' . $agent->device() . '|' . $agent->browser() . '|' . $agent->platform() . '|' . $this->getIpAddress();
+        return md5(session_id() . '|' . $agent->device() . '|' . $agent->browser() . '|' . $agent->platform() . '|' . $agent->languages());
     }
 
     /**
@@ -74,66 +47,6 @@ class OwnSession implements SessionValidator
     {
         $data = $this->createData();
         return ($data === $this->getData());
-    }
-
-    /**
-     * Changes proxy handling setting.
-     *
-     * This must be static method, since validators are recovered automatically
-     * at session read, so this is the only way to switch setting.
-     *
-     * @param bool  $useProxy Whether to check also proxied IP addresses.
-     * @return void
-     */
-    public static function setUseProxy($useProxy = true)
-    {
-        static::$useProxy = $useProxy;
-    }
-
-    /**
-     * Checks proxy handling setting.
-     *
-     * @return bool Current setting value.
-     */
-    public static function getUseProxy()
-    {
-        return static::$useProxy;
-    }
-
-    /**
-     * Set list of trusted proxy addresses
-     *
-     * @param  array $trustedProxies
-     * @return void
-     */
-    public static function setTrustedProxies(array $trustedProxies)
-    {
-        static::$trustedProxies = $trustedProxies;
-    }
-
-    /**
-     * Set the header to introspect for proxy IPs
-     *
-     * @param  string $header
-     * @return void
-     */
-    public static function setProxyHeader($header = 'X-Forwarded-For')
-    {
-        static::$proxyHeader = $header;
-    }
-
-    /**
-     * Returns client IP address.
-     *
-     * @return string IP address.
-     */
-    protected function getIpAddress()
-    {
-        $remoteAddress = new RemoteAddress();
-        $remoteAddress->setUseProxy(static::$useProxy);
-        $remoteAddress->setTrustedProxies(static::$trustedProxies);
-        $remoteAddress->setProxyHeader(static::$proxyHeader);
-        return $remoteAddress->getIpAddress();
     }
 
     /**
